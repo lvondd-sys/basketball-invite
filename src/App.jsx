777b35f29f44b5e4a013fa3b5b34bca69fda4f8d
lvondd-sys <lvondd@icloud.com>
2026-08-18 14:29:56 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const TEAMS = [
   },
   {
     name: "Team 4",
-    players: ["Lucas", "Davy / Pete", "Dave", "Spencer"],
+    players: ["Lucas", "Davy", "Dave / Pete", "Spencer"],
     color: "#44403c",
     bg: "#fafaf9",
   },
